21f267653fe4b95fe12244ce093aec4773e7deaf
JuanD13Perez <diegojuk8@gmail.com>
2022-12-16 20:03:10 -0500

    feat: Video of implementation, csv with test data

--- Mercado_Libre.py
+++ Mercado_Libre.py
@@ -5,49 +5,77 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import pandas as pd
+import numpy as np
 import time
 
 df = pd.read_excel('Products_and_comp.xlsx')
 productos = df['Material']
 competencia = df.columns[3::2]
 
+datos = []
+
 driver = webdriver.Chrome("./chromedriver")
 driver.get("https://www.mercadolibre.com.mx/")
-
-try:
-    search_bar = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "nav-search-input"))
-    )
-    search_bar.clear()
-    search_bar.send_keys(productos[0])
-    search_bar.send_keys(Keys.RETURN)
-
+for i in range(18):
     try:
-        xpath1 = "//h2[@class='ui-search-item__title ui-search-item__group__element shops__items-group-details shops__item-title']"
-        xpath2 = "//h2[@class='ui-search-item__title shops__item-title']"
-        title_products = driver.find_elements(By.XPATH, xpath2)
-        title_products = [title.text for title in title_products]
-        
+        # search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "nav-search-input")))
+        search_bar = driver.find_element(By.CLASS_NAME, "nav-search-input")
+        search_bar.clear()
+        search_bar.send_keys(productos[i])
+        search_bar.send_keys(Keys.RETURN)
 
-        xpathp = "//div[@class='ui-search-price ui-search-price--size-medium shops__price']//span[@class='price-tag-fraction']"
-        price_products = driver.find_elements(By.XPATH,xpathp)
-        price_products = [price.text for price in price_products]
+        try:
+            #xpath2 = "//h2[@class='ui-search-item__title ui-search-item__group__element shops__items-group-details shops__item-title']"
+            xpath = "//h2[@class='ui-search-item__title shops__item-title']"
+            title_products = driver.find_elements(By.XPATH, xpath)
+            title_products = [title.text for title in title_products]
+            
 
-        xpathl = "//div[@class='ui-search-item__group ui-search-item__group--title shops__items-group']//a[1]"
-        links = driver.find_elements(By.XPATH, xpathl)
-        links = [link.get_attribute("href") for link in links]
-        
+            xpathp = "//div[@class='ui-search-price ui-search-price--size-medium shops__price']//span[@class='price-tag ui-search-price__part shops__price-part']//span[@class='price-tag-fraction']"
+            price_products = driver.find_elements(By.XPATH,xpathp)
+            price_products = [price.text for price in price_products]
+
+            xpathl = "//div[@class='ui-search-item__group ui-search-item__group--title shops__items-group']//a[1]"
+            links = driver.find_elements(By.XPATH, xpathl)
+            links = [link.get_attribute("href") for link in links]
+
+            if len(links) == 0 or len(price_products) == 0 or len(title_products) == 0:
+                xpath2 = "//h2[@class='ui-search-item__title ui-search-item__group__element shops__items-group-details shops__item-title']"
+                #xpath = "//h2[@class='ui-search-item__title shops__item-title']"
+                title_products = driver.find_elements(By.XPATH, xpath2)
+                title_products = [title.text for title in title_products]
+                
+
+                xpathp = "//div[@class='ui-search-price ui-search-price--size-medium shops__price']//span[@class='price-tag ui-search-price__part shops__price-part']//span[@class='price-tag-fraction']"
+                price_products = driver.find_elements(By.XPATH,xpathp)
+                price_products = [price.text for price in price_products]
+
+                xpathl = "//div[@class='ui-search-result__wrapper shops__result-wrapper']//div[@class = 'ui-search-result__image shops__picturesStyles']//a[1]"
+                links = driver.find_elements(By.XPATH, xpathl)
+                links = [link.get_attribute("href") for link in links]
+                if len(links) == len(price_products) and len(price_products) == len(title_products) and len(title_products):
+                    products = {'titles': title_products,  'prices': price_products, 'links': links }
+                    df = pd.DataFrame(products)
+                    df['id'] = productos[i]
+                    datos.append(df)
+
+            elif len(links) == len(price_products) and len(price_products) == len(title_products) and len(title_products):
+                products = {'titles': title_products,  'prices': price_products, 'links': links }
+                df = pd.DataFrame(products)
+                df['id'] = productos[i]
+                datos.append(df)
+            else:
+                df = pd.DataFrame({'titles': [np.nan], 'prices': [np.nan], 'links': [np.nan], 'id': productos[i]})
+                print('Product Not Available:' + str(productos[i]) + ' pos ' + str(i))
+                datos.append(df)
+            
+        except:
+            df = pd.DataFrame({'titles': [np.nan], 'prices': [np.nan], 'links': [np.nan], 'id': productos[i]})
+            print('No link for product:' + str(productos[i] + 'pos ' + str(i)))
+            datos.append(df)
     except:
-        print('It was not possible to get titles, prices and links')
+        print('Error in buttom in pos ' + str(i))
 
-    products = {
-        'titles': title_products,
-        'prices': price_products,
-        'links': links
-    }
-
-    df = pd.DataFrame(products)
-    df['id'] = productos[0]
-    df.to_csv('test_ML.csv')
-finally:
-    driver.quit()
+driver.quit() 
+datos_final = pd.concat(datos, ignore_index = True)
+datos_final.to_csv('scrapped_csv/Mercado_Libre.csv')
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # E-commerce Web Scraping
 
+- [**English Version**](#Video)
+- [**Spanish Version**](#Spanish-Version)
+
+
+## Spanish Version
+
 ## Business Problem 📝💹
 
 En este proyecto trabajamos para una compañía de e-commerce que se prepara para un fin de semana de promociones. Como tarea nos fue asignado averigüar los precios que maneja la competencia de algunos productos especiales. Nuestro objetico es **determinar el precio mínimo** que maneja la competencia para cada producto para así poder igualarlo. También queremos los nombres, y los links de la competencia. 
 
-**Nota** Todos los scripts se corrieron en entornos virtuales.
-
-Test 1 superado para Mercado Libre.
-
+**Nota** Todos los scripts se corrieron en un entorno virtual.
 
 Stakeholders: 
 * Marketing manager
@@ -18,6 +21,15 @@ Recibimos:
 
 Entregable:
 * Un pandas dataFrame con los precios y los URL de los productos para cada compañía de la competencia.
+
+A continuación un video con la implementación del algortimo:
+
+### Video
+
+![E-commerce-Video](E-Commerce-Web-Video.mp4)
+
+Cortamos unas partes y aceleramos. Vemos que el algoritmo no es muy rápido ya que usa Selenium. Tampoco pretendemos tener un alto uso de banda ancha. Sobre el final del video vemos los resultados para un test de los primeros 18 links: Un pandas DataFrame con los nombres, links y precios que salen al realizar la búsqueda conjuntamente con un id.
+
 
 ## Requerimientos
  Usamos Python 3.10
